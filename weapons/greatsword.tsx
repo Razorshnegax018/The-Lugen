0856d1f375f1641b2e7554e8561dbcd5f760001d
Razorshnegax018 <63882144+Razorshnegax018@users.noreply.github.com>
2020-12-16 10:55:35 -0600

    Create greatsword.tsx

---
+++ weapons/greatsword.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default class GreatSword extends React.Component{
+  render(){
+    return(
+      <div>
+      <h1>THE GREATSWORD</h1>
+      <sub>The Great Sword is a class 1 Mobility (Slowest) weapon present in all Monster Hunter games</sub>
+      <h3>What is it?</h3>
+      <p><i>As stated in the <a href="https://monsterhunter.fandom.com/wiki/Great_Sword">Monster Hunter Offical Fandom</a> The Great Sword (大剣 Daiken), a classic weapon first introduced in Monster Hunter, is a weapon capable of dealing massive damage. Its way of use has changed since Monster Hunter 2, when it was given a charged attack.  (Monster Hunter Wiki, weapons, Great Sword)</i>
+      <br/>
+      The Great Sword is preemptive weapon that requires absoulute perfect timing, ultimate knowledge of a monster's movement patterns, and complete control of the console, the hands, and the mind. Sounds difficult, somthing that would require peace of mind and months, prehaps years of training to master? It is. But I don't mean to scare you from this insanely powerful weapon. In fact, If you are a Great Sword main yourself, you will know what I mean: You can wipe the floor with monsters, any monster, in 6 minutes or less. And with valor Greatsword giving draw charges the fastcharge skill, more and more people are becoming introduced to the weapon and its might. Even I, a person who never touched Greatsword in <a href="https://monsterhunter.fandom.com/wiki/Monster_Hunter_4_Ultimate" >MH4U</a> used the Greatsword a healthy amount because of valor Greatsword.
+      </p>
+      <h3>What are my main attacks?</h3>
+      <p>The attacks of the greatsword (Guild Style) are as follows:
+      <ul>
+      <li>(Press) X/Triangle: Draw Slash [47 Motion, no mind's eye]</li>
+      <li>(Hold) X/Triangle: Charged Slash [LV1: 47 , LV2: , LV3: , LV3 yes mind's eye]</li>
+      <li>A/Circle: Roundhouse Slash [ Motion, no mind's eye]</li>
+      <li>X + A/Triangle + Circle: Around the back slash [ Motion, yes mind's eye, yes launches players]</li>
+      </ul>
+      <h3>What are the combos I can do?</h3>
+      <ul>
+      <li>Classic Strong Charge: X -> A -> (Hold Circle Pad) X/Triangle -> Circle -> (Hold Circle Pad) Triangle</li>
+      </ul>
+      
+      </p>
+
+      </div>
+    )
+  }
+}
